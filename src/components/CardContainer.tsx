368eaaf4a758b79bff1768553dba23872151b0a2
Ambar Gorgon <ambargorgon@gmail.com>
2025-04-12 19:41:14 -0300

    API Spring boot subida

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,36 +1,43 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Card from '../common/Card';
-import "../styles/cards.css"
+import "../styles/cards.css";
 import { Instrumento } from '../types/instrumento';
 
-  const CardContainer: React.FC = () => {
-    const [instrumentos, setInstrumentos] = useState<Instrumento[]>([]);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      fetch(`http://localhost:8080/instrumentos`)
-        .then((res) => {
-          if (!res.ok) throw new Error('Error al obtener los instrumentos');
-          return res.json();
-        })
-        .then((data) => {
-          setInstrumentos(data);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.error(error);
-          setLoading(false);
-        });
-    }, []);
-  
-    if (loading) return <p>Cargando instrumentos...</p>;
-    return (
-    <div className='card-list'>
-        {instrumentos.map((el, index)=> 
-            <Card key={index} item={el}/>
-        )}
-    </div>
-  )
-}
+const CardContainer: React.FC = () => {
+  const [instrumentos, setInstrumentos] = useState<Instrumento[]>([]);
+  const [loading, setLoading] = useState(true);
 
-export default CardContainer
+  useEffect(() => {
+    fetch(`http://localhost:8080/instrumentos`)
+      .then((res) => {
+        if (!res.ok) throw new Error('Error al obtener los instrumentos');
+        return res.json();
+      })
+      .then((data) => {
+        setInstrumentos(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Cargando instrumentos...</p>;
+
+  return (
+    <>
+      {instrumentos.length === 0 ? (
+        <p>No hay productos disponibles</p>
+      ) : (
+        <div className='card-list'>
+          {instrumentos.map((el, index) => (
+            <Card key={index} item={el} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CardContainer;
